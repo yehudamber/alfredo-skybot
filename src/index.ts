9571ba18511a9fa7e8530f5b52d4306da8bfc61d
Yehuda Bernáth <yehudamber@gmail.com>
2026-09-01 01:20:16 +0300

    End "dry run"

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-const post = await Bot.run(getPostText, { dryRun: true });
+const post = await Bot.run(getPostText);
 
 console.log(
   `[${new Date().toISOString()}] Posted: ${JSON.stringify(post, null, 2)}`
