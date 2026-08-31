99bb629517f7ddf1e439d2ff74edee6541f2c498
Yehuda Bernáth <yehudamber@gmail.com>
2026-08-31 23:11:30 +0300

    Add RTL mark

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,5 +1,6 @@
 import getTerm from "./getTerm.js";
 
 export default async function getPostText() {
-  return {text: `${await getTerm()} ברוטב אלפרדו`, langs: ["he"]};
+  // Add U+200F RIGHT-TO-LEFT MARK to make Latin-script terms render correctly
+  return {text: `\u{200F}${await getTerm()} ברוטב אלפרדו`, langs: ["he"]};
 }
